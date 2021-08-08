776e28e13c86d582e148ed3d26aa5e541b079dfb
Eduard Frigola Bagué <eduardfrigola@gmail.com>
2021-08-08 12:26:06 +0300

    Disable duplication of points

--- src/core/ofxLaserLaser.cpp
+++ src/core/ofxLaserLaser.cpp
@@ -862,7 +862,7 @@ void Laser::send(ofPixels* pixels, float masterIntensity) {
         // if we have a really fast frame, let's duplicate it and reverse it
         // (this helps for things like a single line where we maybe don't want to
         // jump back to the beginning if we can draw the line again reversed)
-        if((pps/ laserPoints.size()) >100) {
+		if(false){//}(pps/ laserPoints.size()) >100) {
             int numpoints = laserPoints.size();
             for(int i = numpoints-1; i>=0; i--) {
                 addPoint(laserPoints[i]);
